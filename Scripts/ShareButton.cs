38d0ce7476bbb6b4ba912f8a47ec7edd8eb24627
Naufal Jundi A <62097278+Ziber7@users.noreply.github.com>
2023-07-16 17:49:21 +0700

    Update ShareButton.cs

--- Scripts/ShareButton.cs
+++ Scripts/ShareButton.cs
@@ -18,6 +18,9 @@ public class ShareButton : MonoBehaviour
         Texture2D ss = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false );
         ss.ReadPixels( new Rect( 0, 0, Screen.width, Screen.height ), 0, 0 );
         ss.Apply();
+        
+        var data = ss.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + "/savedImage.png", data);
 
         string filePath = Path.Combine( Application.temporaryCachePath, "shared img.png" );
         File.WriteAllBytes( filePath, ss.EncodeToPNG() );
